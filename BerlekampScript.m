b36c8381430a2758e7e9b69010fef33dc57b0380
errachete <rafaelherrer@hotmail.es>
2020-01-13 14:41:52 +0100

    Corrected show and normal notation of example

--- BerlekampScript.m
+++ BerlekampScript.m
@@ -5,7 +5,7 @@
 path(path, 'Algorithms');
 path(path, 'Classes');
 clear
-%% $$F_{q}[x] \quad q = 3^{1}$ SDF
+%% $$F_{q}[x] \quad q = 11^{1}$ SDF
 % 
 p = 11;
 n = 1;
@@ -13,15 +13,15 @@ n = 1;
 fqx = FiniteFieldPoly(p,n);
 
 
-f = [0 1 5 1 0 1 0]; %f(x) = 2x + x^3 + x^4 + 2x^5 + x^6
+f = [0 1 5 1 0 1 0]; %f(x) = 1 + 8x + 10x^2 + 8x^3 + x^4 + 8x^5 + x^6
 fprintf("f(x) = %s\n", fqx.gfshow(fqx,f));
 
-berlekamp(f,fqx);
+L = berlekamp(f,fqx);
 
 fprintf("f(x) = ");
 len = length(L);
 for i = 1:len
-    fprintf("(%s)^%i", fqx.gfshow(fqx,L{i}{1}), L{i}{2});
+    fprintf("(%s)", fqx.gfshow(fqx,L{i}));
 end
 fprintf("\n");
 
